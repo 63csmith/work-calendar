9ca2bd542ec0d10f7358389003f60a03ed07fcb5
63csmith <63csmith@gmail.com>
2022-05-18 17:32:14 -0600

    trying to fix js functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-var today = now();
+var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
 
 function hour(){
@@ -25,3 +25,14 @@ function hour(){
     });
 }
 hour();
+
+
+//localStorage
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
